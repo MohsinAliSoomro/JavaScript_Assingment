1f12fa63a2d1727d72e5f020aa7ea39da06945cc
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-06-04 10:43:03 +0500

    Lacture 3 complete

--- app.js
+++ app.js
@@ -1,55 +1,76 @@
-//lacture one 
-//Alert lacture 
+// //lacture one start 
+// //Alert lacture 
 
-//1
-alert('Welcome to our website...');
-//2
-alert('Error! Please enter the valid password');
-//3 
-alert('Welcome to JS Land... \n Happy Coding!')
-//4
-alert('Welcome to JS Land...');
-alert('Happy Coding!');
-//5
-console.log('Hello... I can run JS through my web browsers console ')
-//7 
-//answer { Head }
-//lacture one complete
+// //1
+// alert('Welcome to our website...');
+// //2
+// alert('Error! Please enter the valid password');
+// //3 
+// alert('Welcome to JS Land... \n Happy Coding!')
+// //4
+// alert('Welcome to JS Land...');
+// alert('Happy Coding!');
+// //5
+// console.log('Hello... I can run JS through my web browsers console ')
+// //7 
+// //answer { Head }
+// //lacture one End
 
-//lacture two 
-//variable for string...
+// //lacture two start
+// //variable for string...
 
-//1
-var username;
-//2
-var myName = "Mohsin Ali Soomro";
+// //1
+// var username;
+// //2
+// var myName = "Mohsin Ali Soomro";
+// //3
+// var message="Hello World";
+// alert(message);
+// //4
+// var name ="Jhone Doe";
+// var age ="15 Year Old";
+// var certificate ="Certified Mobile Application Developement";
+// alert(name);
+// alert(age);
+// alert(certificate);
+// //5
+// var pizza= `PIZZA
+// PIZZ
+// PIZ
+// PI
+// P`;
+// alert(pizza);
+// //6
+// var email = "Mohsinalisoomro23@gmail.com";
+// alert("My Email Address is " + email);
+// //7
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn the book " + book);
+// //8
+// document.write("Yeh! I can write HTML content through JavaScript");
+// //9
+// var desing ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(desing)
+// //lacture two End
+
+// //lacture three start
+// //Variable for Numbers
+
+// //1
+// var age = 22;
+// alert(age);
+// //2
+// var visited;
+// visited = 100;
+// alert(`You Have visited this site ${visited} times`);
 //3
-var message="Hello World";
-alert(message);
+var birthYear=1998;
+document.write("My Birth Year is " + birthYear +"<br />");
+document.write("Data Type of my declared variable is " + typeof(birthYear));
 //4
-var name ="Jhone Doe";
-var age ="15 Year Old";
-var certificate ="Certified Mobile Application Developement";
-alert(name);
-alert(age);
-alert(certificate);
-//5
-var pizza= `PIZZA
-PIZZ
-PIZ
-PI
-P`;
-alert(pizza);
-//6
-var email = "Mohsinalisoomro23@gmail.com";
-alert("My Email Address is " + email);
-//7
-var book = "A smarter way to learn JavaScript"
-alert("I am trying to learn the book " + book);
-//8
-document.write("Yeh! I can write HTML content through JavaScript");
-//9
-var desing ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
-alert(desing)
-//lacture two complete
-
+var visitorname ="Jhone Doe";
+var producttitle="T-shirt";
+var storename ="XYZ Clothes Store";
+var quintity=5;
+document.write(`<br /> ${visitorname} ordered ${quintity} ${producttitle}(s) on ${storename}`);
+//lacture three End
