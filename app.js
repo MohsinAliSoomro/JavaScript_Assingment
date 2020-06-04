250e3a8a9f429a5106457771db5c2e18922e21ce
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-06-04 09:54:39 +0500

    Chapter one complete

---
+++ app.js
@@ -0,0 +1,15 @@
+//lacture one 
+//Alert lacture 
+//1
+alert('Welcome to our website...');
+//2
+alert('Error! Please enter the valid password');
+//3 
+alert('Welcome to JS Land... \n Happy Coding!')
+//4
+alert('Welcome to JS Land...');
+alert('Happy Coding!');
+//5
+console.log('Hello... I can run JS through my web browsers console ')
+//7 
+//answer { Head }
