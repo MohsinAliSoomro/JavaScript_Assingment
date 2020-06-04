58b0c9ad69b9b6d39662544c5ea93b6ebc603d16
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-06-04 10:23:01 +0500

    Two Lacture Complete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //lacture one 
 //Alert lacture 
+
 //1
 alert('Welcome to our website...');
 //2
@@ -13,3 +14,42 @@ alert('Happy Coding!');
 console.log('Hello... I can run JS through my web browsers console ')
 //7 
 //answer { Head }
+//lacture one complete
+
+//lacture two 
+//variable for string...
+
+//1
+var username;
+//2
+var myName = "Mohsin Ali Soomro";
+//3
+var message="Hello World";
+alert(message);
+//4
+var name ="Jhone Doe";
+var age ="15 Year Old";
+var certificate ="Certified Mobile Application Developement";
+alert(name);
+alert(age);
+alert(certificate);
+//5
+var pizza= `PIZZA
+PIZZ
+PIZ
+PI
+P`;
+alert(pizza);
+//6
+var email = "Mohsinalisoomro23@gmail.com";
+alert("My Email Address is " + email);
+//7
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn the book " + book);
+//8
+document.write("Yeh! I can write HTML content through JavaScript");
+//9
+var desing ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(desing)
+//lacture two complete
+
