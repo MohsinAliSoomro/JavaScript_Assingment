8cfd394a24c345ebdc29a711970772c68bc660cb
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-06-04 11:29:46 +0500

    start from temprature converter

--- app.js
+++ app.js
@@ -105,3 +105,71 @@ document.write("<br>Variable names are case Sensitive");
 //E
 document.write("<br>Variable names should not be JS keyword");
 
+//Lacture 4 End
+
+//lacture 5 start
+//Math Expressions
+
+//1 
+// Add
+var a=1;
+var b=2;
+var c=a+b;
+document.write(`Sum of ${a} and ${b} is ${c}`);
+//2
+// Sub
+var a=1;
+var b=2;
+var c=a-b;
+document.write(`substract of ${a} and ${b} is ${c}`);
+// Mul
+var a=1;
+var b=2;
+var c=a*b;
+document.write(`Multiply of ${a} and ${b} is ${c}`);
+// Div
+var a=1;
+var b=2;
+var c=a/b;
+document.write(`division of ${a} and ${b} is ${c}`);
+//3
+//a
+var value;
+//b
+document.write(`<br />Value after variable declaration is ${value}`);
+//c
+value=5;
+//d
+document.write(`<br />Initial value ${value}`);
+//e
+value++;
+//f
+document.write(`<br />Value after increment is ${value}`);
+//g
+value = value+7;
+//H
+document.write(`<br />Value after addition is 7`);
+//i
+value--;
+//j
+document.write(`<br />Value after decrement is ${value}`);
+//k
+value=value%3;
+//L
+document.write(`<br />The remainder is ${value}`)
+//4
+var ticket = 600;
+ticket=ticket*5;
+document.write(`<br/>Total cost to buy 5 tickets to a movie is ${ticket} PKR`);
+//5
+document.write(`<br/>table of 4`)
+document.write(`<br/>4x1=4`);
+document.write(`<br/>4x2=8`);
+document.write(`<br/>4x3=12`);
+document.write(`<br/>4x4=16`);
+document.write(`<br/>4x5=20`);
+document.write(`<br/>4x6=24`);
+document.write(`<br/>4x7=28`);
+document.write(`<br/>4x8=32`);
+document.write(`<br/>4x9=36`);
+document.write(`<br/>4x10=40`);
